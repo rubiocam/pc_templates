539b8dcc39e879b9c02b6199b38720c1e48c7be2
Samantha Rubio-Campos <sam_rubioc05@yahoo.com>
2026-08-02 14:26:57 -0700

    updating name of repository

--- pc-templates/vite.config.js
+++ pc-templates/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rubiocam.github.io/pc_templates/"
+  base: "https://rubiocam.github.io/extra-cards/"
 })
